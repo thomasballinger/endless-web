26956290e34ae3bd0530ccd1fc877b117889d00e
Michał Janiszewski <janisozaur+signed@gmail.com>
2020-07-12 23:08:29 +0200

    HTML for web build
    Add basic shell file
    - Themed HTML loading screen
    - Cache endless-sky.data with IndexedDB
    - Allow upload of save games with HTML interface
    - Load script manually, no shell html file.
    - Allow downloading of save files
    - Clean up HTML interface
    - Add progress bar text during data download.
    - Persist save files to IndexedDB
    - Check if WebGL2 supported before downloading data
    - Check hash and length of data and store in dataversion.js
    - Report errors to stdout (#22)
    
    disable music in web build

--- source/GameWindow.cpp
+++ source/GameWindow.cpp
@@ -424,6 +424,7 @@ void GameWindow::ExitWithError(const string& message, bool doPopUp)
 		box.title = "Endless Sky: Error";
 		box.message = message.c_str();
 		box.colorScheme = nullptr;
+		printf("Error: %s\n", message.c_str());
 		
 		SDL_MessageBoxButtonData button;
 		button.flags = SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT;
