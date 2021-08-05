4b8fb215e7f5b0d4fffebc39561f2dd13033eabb
Thomas Ballinger <thomasballinger@gmail.com>
2021-08-04 21:07:40 -0700

    Disable music in Emscripten

--- source/Audio.cpp
+++ source/Audio.cpp
@@ -171,6 +171,9 @@ void Audio::Init(const vector<string> &sources)
 #endif
 	
 	// Create the music-streaming threads.
+#ifdef __EMSCRIPTEN__
+	return; // Return early because Emscripten doesn't like threads! (and uses a no-op libmad mock)
+#endif
 	currentTrack.reset(new Music());
 	previousTrack.reset(new Music());
 	alGenSources(1, &musicSource);
@@ -293,6 +296,9 @@ void Audio::Play(const Sound *sound, const Point &position)
 // Play the given music. An empty string means to play nothing.
 void Audio::PlayMusic(const string &name)
 {
+#ifdef __EMSCRIPTEN__
+	return; // Return early because Emscripten doesn't like threads! (and uses a no-op libmad mock)
+#endif
 	if(!isInitialized)
 		return;
 
@@ -404,7 +410,10 @@ void Audio::Step()
 		alSourcePlay(source);
 	}
 	queue.clear();
-
+	
+#ifdef __EMSCRIPTEN__
+	return; // Return early because Emscripten doesn't like threads! (and uses a no-op libmad mock)
+#endif
 	// Queue up new buffers for the music, if necessary.
 	int buffersDone = 0;
 	alGetSourcei(musicSource, AL_BUFFERS_PROCESSED, &buffersDone);
@@ -491,6 +500,7 @@ void Audio::Quit()
 	sounds.clear();
 
 	// Clean up the music source and buffers.
+#ifndef __EMSCRIPTEN__
 	if(isInitialized)
 	{
 		alSourceStop(musicSource);
@@ -499,6 +509,7 @@ void Audio::Quit()
 		currentTrack.reset();
 		previousTrack.reset();
 	}
+#endif
 
 	// Close the connection to the OpenAL library.
 	if(context)
