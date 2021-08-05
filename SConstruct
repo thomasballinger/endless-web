thomasballinger/endless-sky

HTML for web build
Add basic shell file
- Themed HTML loading screen
- Cache endless-sky.data with IndexedDB
- Allow upload of save games with HTML interface
- Load script manually, no shell html file.
- Allow downloading of save files
- Clean up HTML interface
- Add progress bar text during data download.
- Persist save files to IndexedDB
- Check if WebGL2 supported before downloading data
- Check hash and length of data and store in dataversion.js
- Report errors to stdout (#22)

SConstruct
@@ -126,6 +126,7 @@ if env["mode"] == "emcc":
 		"-s", "WASM_MEM_MAX=2147483648", # 2GB
 		"-s", "INITIAL_MEMORY=838860800", # 800MB
 		"-s", "ALLOW_MEMORY_GROWTH=1",
+		"-s", "EXTRA_EXPORTED_RUNTIME_METHODS=['callMain']",
 		"--preload-file", "data",
 		"--preload-file", "images",
 		"--preload-file", "sounds",
@@ -193,15 +194,44 @@ def RecursiveGlob(pattern, dir_name=buildDirectory):
 
 # By default, invoking scons will build the backing archive file and then the game binary.
 sourceLib = env.StaticLibrary(pathjoin(libDirectory, "endless-sky"), RecursiveGlob("*.cpp", buildDirectory))
-outname = "endless-sky"
-if env["mode"] == "emcc":
-    outname += ".html"
 exeObjs = [Glob(pathjoin(buildDirectory, f)) for f in ("main.cpp",)]
 if is_windows_host:
 	windows_icon = env.RES(pathjoin(buildDirectory, "WinApp.rc"))
 	exeObjs.append(windows_icon)
-sky = env.Program(pathjoin(binDirectory, outname), exeObjs + sourceLib)
-env.Default(sky)
+
+def create_data_version_javascript(env, target, source):
+	import hashlib
+	hash_md5 = hashlib.md5()
+	length = 0
+	with open(str(source[0]), "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b""):
+			length += len(chunk)
+			hash_md5.update(chunk)
+	hash = hash_md5.hexdigest()
+
+	with open(str(target[0]), 'w') as f:
+		f.write('// autogenerated file, do not edit\n')
+		f.write('// This is the md5 hash of the data file expected\n')
+		f.write('var endlessSkyDataVersion = "')
+		f.write(hash)
+		f.write('";\nvar endlessSkyDataSize = ')
+		f.write(str(length))
+		f.write(';\n')
+
+if env["mode"] == "emcc":
+	sky = env.Program(['endless-sky.js', 'endless-sky.wasm', 'endless-sky.data'], exeObjs + sourceLib)
+	title = env.Command("title.png", "images/_menu/title.png", Copy("$TARGET", "$SOURCE"))
+	env.AlwaysBuild(title)
+	dataversion = env.Command(
+		target="dataversion.js",
+		source="endless-sky.data",
+		action=create_data_version_javascript)
+	env.Depends(dataversion, sky)
+	env.Depends(dataversion, title)
+	env.Default(dataversion)
+else:
+	sky = env.Program(pathjoin(binDirectory, 'endless-sky'), exeObjs + sourceLib)
+	env.Default(sky)
 
 
 # The testing infrastructure ignores "mode" specification (i.e. we only test optimized output).
@@ -229,7 +259,6 @@ catch2_args = " " + " ".join([
 test_runner = env.Action(test[0].abspath + catch2_args, 'Running tests...')
 env.Alias("test", test, test_runner)
 env.AlwaysBuild("test")
-
 
 # Install the binary:
 env.Install("$DESTDIR$PREFIX/games", sky)

thomasballinger/endless-web

SConstruct
@@ -142,6 +142,7 @@ if env["mode"] == "emcc":
 		"-s", "WASM_MEM_MAX=2147483648", # 2GB
 		"-s", "INITIAL_MEMORY=629145600", # 600MB
 		"-s", "ALLOW_MEMORY_GROWTH=1",
+		"-s", "EXPORTED_RUNTIME_METHODS=['callMain']",
 
 		# load these directories in a virtual filesystem
 		"--preload-file", "data",
@@ -204,15 +205,44 @@ def RecursiveGlob(pattern, dir_name=buildDirectory):
 
 # By default, invoking scons will build the backing archive file and then the game binary.
 sourceLib = env.StaticLibrary(pathjoin(libDirectory, "endless-sky"), RecursiveGlob("*.cpp", buildDirectory))
-outname = "endless-sky"
-if env["mode"] == "emcc":
-    outname += ".html"
 exeObjs = [Glob(pathjoin(buildDirectory, f)) for f in ("main.cpp",)]
 if is_windows_host:
 	windows_icon = env.RES(pathjoin(buildDirectory, "WinApp.rc"))
 	exeObjs.append(windows_icon)
-sky = env.Program(pathjoin(binDirectory, outname), exeObjs + sourceLib)
-env.Default(sky)
+
+def create_data_version_javascript(env, target, source):
+	import hashlib
+	hash_md5 = hashlib.md5()
+	length = 0
+	with open(str(source[0]), "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b""):
+			length += len(chunk)
+			hash_md5.update(chunk)
+	hash = hash_md5.hexdigest()
+
+	with open(str(target[0]), 'w') as f:
+		f.write('// autogenerated file, do not edit\n')
+		f.write('// This is the md5 hash of the data file expected\n')
+		f.write('var endlessSkyDataVersion = "')
+		f.write(hash)
+		f.write('";\nvar endlessSkyDataSize = ')
+		f.write(str(length))
+		f.write(';\n')
+
+if env["mode"] == "emcc":
+	sky = env.Program(['endless-sky.js', 'endless-sky.wasm', 'endless-sky.data'], exeObjs + sourceLib)
+	title = env.Command("title.png", "images/_menu/title.png", Copy("$TARGET", "$SOURCE"))
+	env.AlwaysBuild(title)
+	dataversion = env.Command(
+		target="dataversion.js",
+		source="endless-sky.data",
+		action=create_data_version_javascript)
+	env.Depends(dataversion, sky)
+	env.Depends(dataversion, title)
+	env.Default(dataversion)
+else:
+	sky = env.Program(pathjoin(binDirectory, 'endless-sky'), exeObjs + sourceLib)
+	env.Default(sky)
 
 
 # The testing infrastructure ignores "mode" specification (i.e. we only test optimized output).