2e1c5a76d325d16127976f253e2c96b2f8a6d7fc
Thomas Ballinger <thomasballinger@gmail.com>
2021-08-28 09:51:44 -0700

    remove dynamic cast (this may need to be backported)

--- source/Test.cpp
+++ source/Test.cpp
@@ -471,7 +471,7 @@ void Test::Step(Context &context, UI &menuPanels, UI &gamePanels, PlayerInfo &pl
 					
 					// Both get as well as the cast can result in a nullpointer. In both cases we
 					// will fail the test, since we expect the MainPanel to be here.
-					auto mainPanel = dynamic_cast<MainPanel *>(gamePanels.Root().get());
+					auto mainPanel = static_cast<MainPanel *>(gamePanels.Root().get());
 					if(!mainPanel)
 						Fail(context, player, "root gamepanel of wrong type when sending command");
 
